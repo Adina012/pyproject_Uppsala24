62f1b62dcf7e3658e018f140bd13a2bea1f8a4a4
Adina012 <adina.landstrom@gmail.com>
2024-03-07 15:29:54 +0100

    Commiting last changes, code is running but no values are added to the list sign_change_indices...currently trying to figure out that.

--- battery_cycling.py
+++ battery_cycling.py
@@ -11,9 +11,9 @@ sign_change_indices = []
 # Iterate over the series
 for i in range(1, len(df)):
     # Check if the sign changes
-    if (df['Current(mA)'].iloc[i] >= 0 and df['Current(mA)'].iloc[i-1] < 0) or (df['Current(mA)'].iloc[i] < 0 and df['Current(mA)'].iloc[i-1] >= 0):
+    if (df.iloc[i,5] >= 0 and df.iloc[i+1,5] < 0) or (df.iloc[i,5] < 0 and df.iloc[i+1,5] >= 0):
         sign_change_indices.append(i)
-
+print(sign_change_indices)
 print("What do you want to plot on the x- and y-axis?"
       "1:time(s),2:Potential(V),3:Capacity(mAh),4:Current(mA)")
 x = int(input())
@@ -29,14 +29,12 @@ if no_de_lithiation <= 0 or no_de_lithiation > len(sign_change_indices):
     print("Invalid lithiation/delithiation number.")
 else:
     # Extract the start and end indices for the chosen lithiation/delithiation
-    start_index = sign_change_indices[no_de_lithiation - 1]
-    if no_de_lithiation == len(sign_change_indices):
-        end_index = len(df)
-    else:
-        end_index = sign_change_indices[no_de_lithiation]
+    start_index = sign_change_indices[no_de_lithiation]
+    end_index = sign_change_indices[no_de_lithiation+1]
+    plt.plot(df.iloc[start_index:end_index,x], df.iloc[start_index:end_index,y])
 
     # Plot the selected data
-    plt.plot(df.iloc[start_index:end_index, x], df.iloc[start_index:end_index, y])
+   #  plt.plot(df.iloc[start_index:end_index, x], df.iloc[start_index:end_index, y])
     plt.xlabel(df.columns[x])
     plt.ylabel(df.columns[y])
     plt.title(f"Lithiation/Delithiation {no_de_lithiation}")
