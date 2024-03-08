0d1dac5f63416c5f21e57ee684e30b2a39a88819
Adina012 <adina.landstrom@gmail.com>
2024-03-08 13:18:20 +0100

    Added description of code in main

--- battery_cycling.py
+++ battery_cycling.py
@@ -1,6 +1,14 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+'''
+This code helps you to plot the experimental data from lithiation/delithiation of a Li half-cell. First it loads a txt-file 
+with the data and converts it to a data-frame.It will then create a list of the indices of the points were the sign of the 
+current changes. This list will make it possible to choose which lithiation/delithiation you want to plot.
+Then you are asked what you want to plot on the respective axis:
+1:time(s),2:Potential(V),3:Capacity(mAh),4:Current(mA). 
+'''
+
 # Load the data
 data = pd.read_csv('AL_lithiated141_3form1ma_C08.txt', delimiter="\t", decimal=",")
 df = pd.DataFrame(data)
